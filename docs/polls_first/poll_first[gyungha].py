52556a3e56ef22857049bd398f30fa7c50d27012
rugkfl <kimgyoungha61@gmail.com>
2023-12-07 09:15:05 +0900

    ..

--- docs/polls_first/poll_first[gyungha].py
+++ docs/polls_first/poll_first[gyungha].py
@@ -31,8 +31,8 @@ list_answer =  ["좋음", "중간", "좋아지길"]
 
 
 for num_question in range(len(list_question)) :           
-    print("{} .{}".format(num_question + 1, list_question[num_question])) # 이 부분은 'list_question'이라는 리스트의 길이만큼 반복을 할 것이라는 뜻 /# 'len' 함수는 리스트의 길이를 반환하고, 'range' 함수는 0부터 인자로 받은 숫자 - 1 까지의 일련의 정수를 반환 /  #따라서 이 코드는 'list_question'의 모든 요소에 대하여 반복을 수행
-    for num_answer in range(len(list_answer)) :  # 반복문으로, 'list_answer'라는 리스트의 길이만큼 반복을 할 것이라는 뜻. 이 코드는 'list_answer'의 모든 요소에 대하여 반복을 수행
+    print("{} .{}".format(num_question + 1, list_question[num_question])) 
+    for num_answer in range(len(list_answer)) :  
         print("{}. {}".format(num_answer + 1, list_answer[num_answer]), end="  ")
     if num_question != 3 :
         print("")
@@ -41,8 +41,9 @@ for num_question in range(len(list_question)) :
         break
 
 
+# 이 부분은 'list_question'이라는 리스트의 길이만큼 반복을 할 것이라는 뜻 /# 'len' 함수는 리스트의 길이를 반환하고, 'range' 함수는 0부터 인자로 받은 숫자 - 1 까지의 일련의 정수를 반환 /  #따라서 이 코드는 'list_question'의 모든 요소에 대하여 반복을 수행
 
-
+ # 반복문으로, 'list_answer'라는 리스트의 길이만큼 반복을 할 것이라는 뜻. 이 코드는 'list_answer'의 모든 요소에 대하여 반복을 수행
 
 #  이 코드는 'list_answer'의 각 요소를 순서대로 화면에 출력하며, 각 요소 앞에는 1부터 시작하는 숫자가 붙습니다. 'end="  "'은 print 함수가 끝나고 나서 줄바꿈이 아닌 두 번의 공백("  ")을 출력하라는 의미입니다.
 
